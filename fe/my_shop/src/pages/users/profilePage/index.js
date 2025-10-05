bbb209681c236c354cc282bc99d0f358b736f222
nguyenketri <nguyenketri1803@gmail.com>
2025-10-05 16:01:49 +0700

    Init Custom Router

---
+++ fe/my_shop/src/pages/users/profilePage/index.js
@@ -0,0 +1,11 @@
+import {memo} from "react"
+
+const ProFilePage = () => {
+    return(
+        <>
+      
+         <h1>Profile Page</h1>
+        </>
+    )
+}
+export default memo(ProFilePage);
---
+++ fe/my_shop/src/router.js
@@ -0,0 +1,38 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/users/homePage";
+import { ROUTERS } from "./utils/router";
+import MasterLayout from "./pages/users/theme/masterLayout";
+import ProfilePage from "./pages/users/profilePage";
+
+const renderUserRouter = () => {
+    const userRouter = [
+        {
+            path: ROUTERS.USER.HOME,
+            component: <HomePage/>
+        },
+          {
+            path: ROUTERS.USER.PROFILE,
+            component: <ProfilePage/>
+        }
+    ]
+
+    return(
+        <MasterLayout>
+        <Routes>
+           {
+            userRouter.map((item,key) => 
+              <Route key={key} path={item.path}
+                element= {item.component}
+                />
+            )
+           }
+        </Routes>
+        </MasterLayout>
+    )
+}
+
+const RouterCustom = () => {
+    return renderUserRouter();
+};
+
+export default RouterCustom
